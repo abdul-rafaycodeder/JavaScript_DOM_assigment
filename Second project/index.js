bb7d6798c1fa2cc8148d5a8ad2286273e85610b7
abdul rafay <abdulrafaycodeder@email.com>
2026-01-07 09:53:01 -0800

    kar raha ho coding

--- Second project/index.js
+++ Second project/index.js
@@ -1,21 +1,23 @@
 function CheckPrimeNumber() {
-   var num = Number(document.getElementById("numberInput").value);
-   var result = document.getElementById("result");
-   if (num <= 1) {
-       result.textContent = "Not a prime number";
-   } else {
-       var isPrime = true;
-       for (var i = 2; i < num; i++) {
-           if (num % i === 0) {
-               isPrime = false;
-               break;
-           }
-       }
+    var num = Number(document.getElementById("numberInput").value);
+    var result = document.getElementById("result");
+    if (num <= 1) {
+        result.textContent = "Not a prime number";
+    } else {
+        var isPrime = true;
+        for (var i = 2; i < num; i++) {
+            if (num % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
 
-       if (isPrime) {
-           result.textContent = "Prime number";
-       } else {
-           result.textContent = "Not a prime number";
-       }
-   }
+
+        if (isPrime) {
+            result.textContent = "Prime number";
+        } else {
+            result.textContent = "Not a prime number";
+        }
+    }
+
 }
