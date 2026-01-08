c3f4fcc8d303bc777cfa69c297d0eb08795b4ee5
abdul rafay <abdulrafaycodeder@email.com>
2026-01-08 04:17:42 -0800

    mini changes

--- Second project/index.js
+++ Second project/index.js
@@ -1,23 +1,29 @@
 function CheckPrimeNumber() {
-    var num = Number(document.getElementById("numberInput").value);
+    var input = document.getElementById("numberInput").value;
     var result = document.getElementById("result");
+
+     if (input === "") {
+        result.innerText = "Please enter a number ❌";
+        result.style.color = "red";
+        return;
+    }
+
+    var num = Number(input);
+
     if (num <= 1) {
-        result.textContent = "Not a prime number";
-    } else {
-        var isPrime = true;
-        for (var i = 2; i < num; i++) {
-            if (num % i === 0) {
-                isPrime = false;
-                break;
-            }
-        }
+        result.innerText = "Not a prime number";
+        result.style.color = "black";
+        return;
+    }
 
-
-        if (isPrime) {
-            result.textContent = "Prime number";
-        } else {
-            result.textContent = "Not a prime number";
+    for (var i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            result.innerText = "Not a prime number";
+            result.style.color = "red";
+            return;
         }
     }
 
+    result.innerText = "Prime number ✔️";
+    result.style.color = "green";
 }
